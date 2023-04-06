a8df7ddac163e3362c87568a225647d2e8a5ddc0
Gengorou-C <90015823+Gengorou-C@users.noreply.github.com>
2023-04-06 13:05:46 +0900

    プレイヤー以外に死亡モーション追加

--- src/main/java/com/kAIS/KAIMyEntity/mixin/KAIMyEntityPlayerRendererMixin.java
+++ src/main/java/com/kAIS/KAIMyEntity/mixin/KAIMyEntityPlayerRendererMixin.java
@@ -48,14 +48,16 @@ public abstract class KAIMyEntityPlayerRendererMixin extends LivingEntityRendere
         float bodyPitch = 0.0f;
         Vec3f entityTrans = new Vec3f(0.0f, 0.0f, 0.0f);
         MMDModelManager.Model m = MMDModelManager.GetPlayerModel("EntityPlayer_" + entityIn.getName().getString());
-        if (m == null)
+        if (m == null){
             m = MMDModelManager.GetPlayerModel("EntityPlayer");
+        }        
         if (m == null){
             super.render(entityIn, entityYaw, partialTicks, matrixStackIn, vertexConsumers, packedLightIn);
             return;
         } 
-        if (m != null)
+        if (m != null){
             model = m.model;
+        }
 
         MMDModelManager.ModelWithPlayerData mwpd = (MMDModelManager.ModelWithPlayerData) m;
         mwpd.loadModelProperties(KAIMyEntityClient.reloadProperties);
@@ -67,6 +69,7 @@ public abstract class KAIMyEntityPlayerRendererMixin extends LivingEntityRendere
         Vec3f swimmingTrans = mwpd.properties.getProperty("swimmingTrans") == null ? new Vec3f(0.0f, 0.0f, 0.0f) : KAIMyEntityClient.str2Vec3f(mwpd.properties.getProperty("swimmingTrans"));
         float crawlingPitch = mwpd.properties.getProperty("crawlingPitch") == null ? 0.0f : Float.valueOf(mwpd.properties.getProperty("crawlingPitch"));
         Vec3f crawlingTrans = mwpd.properties.getProperty("crawlingTrans") == null ? new Vec3f(0.0f, 0.0f, 0.0f) : KAIMyEntityClient.str2Vec3f(mwpd.properties.getProperty("crawlingTrans"));
+        float[] size = sizeOfModel(mwpd);
 
         if (model != null) {
             if (!mwpd.playerData.playCustomAnim) {
@@ -153,9 +156,6 @@ public abstract class KAIMyEntityPlayerRendererMixin extends LivingEntityRendere
                 }
             }
 
-            float[] size = sizeOfModel(mwpd);
-            if(KAIMyEntityClient.reloadProperties)
-                KAIMyEntityClient.reloadProperties = false;
             if(KAIMyEntityClient.calledFrom(6).contains("Inventory") || KAIMyEntityClient.calledFrom(6).contains("class_490")){ // net.minecraft.class_490 == net.minecraft.client.gui.screen.ingame.InventoryScreen
                 RenderSystem.setShader(GameRenderer::getPositionTexShader);
                 MatrixStack PTS_modelViewStack = RenderSystem.getModelViewStack();
--- src/main/java/com/kAIS/KAIMyEntity/renderer/KAIMyEntityRenderer.java
+++ src/main/java/com/kAIS/KAIMyEntity/renderer/KAIMyEntityRenderer.java
@@ -33,63 +33,73 @@ public class KAIMyEntityRenderer<T extends Entity> extends EntityRenderer<T> {
     @Override
     public void render(T entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, VertexConsumerProvider bufferIn, int packedLightIn) {
         super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
-        String animName;
+        String animName = "";
         float bodyYaw = entityYaw;
         float bodyPitch = 0.0f;
         Vec3f entityTrans = new Vec3f(0.0f, 0.0f, 0.0f);
-        if (entityIn.hasPassengers() && (entityIn.getX() - entityIn.prevX != 0.0f || entityIn.getZ() - entityIn.prevZ != 0.0f)) {
-            animName = "driven";
-        } else if (entityIn.hasPassengers()) {
-            animName = "ridden";
-        } else if (entityIn.isSwimming()) {
-            animName = "swim";
-        } else if ( (entityIn.getX() - entityIn.prevX != 0.0f || entityIn.getZ() - entityIn.prevZ != 0.0f) && entityIn.getVehicle() == null) {
-            animName = "walk";
-        } else {
-            animName = "idle";
+        MMDModelManager.Model model = MMDModelManager.GetModel(modelName, entityIn.getUuidAsString());
+        if(model == null){
+            return;
         }
-        MMDModelManager.Model model = MMDModelManager.GetNotPlayerModel(modelName, animName);
+        MMDModelManager.ModelWithEntityData mwed = (MMDModelManager.ModelWithEntityData)model;
         model.loadModelProperties(false);
-        float sleepingPitch = model.properties.getProperty("sleepingPitch") == null ? 0.0f : Float.valueOf(model.properties.getProperty("sleepingPitch"));
-        Vec3f sleepingTrans = model.properties.getProperty("sleepingTrans") == null ? new Vec3f(0.0f,0.0f,0.0f) : KAIMyEntityClient.str2Vec3f(model.properties.getProperty("sleepingTrans"));
         float[] size = sizeOfModel(model);
-        if (model != null) {
-            matrixStackIn.push();
-            if(entityIn instanceof LivingEntity){
-                if(((LivingEntity) entityIn).isSleeping()){
-                    animName = "sleep";
-                    bodyYaw = ((LivingEntity) entityIn).getSleepingDirection().asRotation() + 180.0f;
-                    bodyPitch = sleepingPitch;
-                    entityTrans = sleepingTrans;
-                }
-                if(((LivingEntity) entityIn).isBaby()){
-                    matrixStackIn.scale(0.5f, 0.5f, 0.5f);
-                }
+        matrixStackIn.push();
+        if(entityIn instanceof LivingEntity){
+            if(((LivingEntity) entityIn).getHealth() <= 0.0F){
+                animName = "die";
+                AnimStateChangeOnce(mwed, MMDModelManager.EntityData.EntityState.Die, 0);
+            }else if(((LivingEntity) entityIn).isSleeping()){
+                animName = "sleep";
+                bodyYaw = ((LivingEntity) entityIn).getSleepingDirection().asRotation() + 180.0f;
+                bodyPitch = model.properties.getProperty("sleepingPitch") == null ? 0.0f : Float.valueOf(model.properties.getProperty("sleepingPitch"));
+                entityTrans = model.properties.getProperty("sleepingTrans") == null ? new Vec3f(0.0f, 0.0f, 0.0f) : KAIMyEntityClient.str2Vec3f(model.properties.getProperty("sleepingTrans"));
+                AnimStateChangeOnce(mwed, MMDModelManager.EntityData.EntityState.Sleep, 0);
             }
-
-            if(KAIMyEntityClient.calledFrom(6).contains("Inventory") || KAIMyEntityClient.calledFrom(6).contains("class_490")){ // net.minecraft.class_490 == net.minecraft.client.gui.screen.ingame.InventoryScreen
-                RenderSystem.setShader(GameRenderer::getPositionTexShader);
-                MatrixStack PTS_modelViewStack = RenderSystem.getModelViewStack();
-                PTS_modelViewStack.translate(0.0f, 0.0f, 1000.0f);
-                PTS_modelViewStack.push();
-                PTS_modelViewStack.scale(20.0f,20.0f, 20.0f);
-                PTS_modelViewStack.scale(size[1], size[1], size[1]);
-                Quaternion quaternion = Vec3f.POSITIVE_Z.getDegreesQuaternion(180.0f);
-                Quaternion quaternion1 = Vec3f.POSITIVE_X.getDegreesQuaternion(-entityIn.getPitch());
-                Quaternion quaternion2 = Vec3f.POSITIVE_Y.getDegreesQuaternion(-entityIn.getYaw());
-                quaternion.hamiltonProduct(quaternion1);
-                quaternion.hamiltonProduct(quaternion2);
-                PTS_modelViewStack.multiply(quaternion);
-                RenderSystem.setShader(GameRenderer::getRenderTypeEntityCutoutNoNullShader);
-                model.model.Render(entityIn, entityYaw, 0.0f, new Vec3f(0.0f,0.0f,0.0f), PTS_modelViewStack, packedLightIn);
-                PTS_modelViewStack.pop();
-            }else{
-                matrixStackIn.scale(size[0], size[0], size[0]);
-                RenderSystem.setShader(GameRenderer::getRenderTypeEntityCutoutNoNullShader);
-                model.model.Render(entityIn, bodyYaw, bodyPitch, entityTrans, matrixStackIn, packedLightIn);
+            if(((LivingEntity) entityIn).isBaby()){
+                matrixStackIn.scale(0.5f, 0.5f, 0.5f);
             }
-            matrixStackIn.pop();
         }
+        if(animName == ""){
+            if (entityIn.hasPassengers() && (entityIn.getX() - entityIn.prevX != 0.0f || entityIn.getZ() - entityIn.prevZ != 0.0f)) {
+                animName = "driven";
+                AnimStateChangeOnce(mwed, MMDModelManager.EntityData.EntityState.Driven, 0);
+            } else if (entityIn.hasPassengers()) {
+                animName = "ridden";
+                AnimStateChangeOnce(mwed, MMDModelManager.EntityData.EntityState.Ridden, 0);
+            } else if (entityIn.isSwimming()) {
+                animName = "swim";
+                AnimStateChangeOnce(mwed, MMDModelManager.EntityData.EntityState.Swim, 0);
+            } else if ( (entityIn.getX() - entityIn.prevX != 0.0f || entityIn.getZ() - entityIn.prevZ != 0.0f) && entityIn.getVehicle() == null) {
+                animName = "walk";
+                AnimStateChangeOnce(mwed, MMDModelManager.EntityData.EntityState.Walk, 0);
+            } else {
+                animName = "idle";
+                AnimStateChangeOnce(mwed, MMDModelManager.EntityData.EntityState.Idle, 0);
+            }
+        }
+        if(KAIMyEntityClient.calledFrom(6).contains("Inventory") || KAIMyEntityClient.calledFrom(6).contains("class_490")){ // net.minecraft.class_490 == net.minecraft.client.gui.screen.ingame.InventoryScreen
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            MatrixStack PTS_modelViewStack = RenderSystem.getModelViewStack();
+            PTS_modelViewStack.translate(0.0f, 0.0f, 1000.0f);
+            PTS_modelViewStack.push();
+            PTS_modelViewStack.scale(20.0f,20.0f, 20.0f);
+            PTS_modelViewStack.scale(size[1], size[1], size[1]);
+            Quaternion quaternion = Vec3f.POSITIVE_Z.getDegreesQuaternion(180.0f);
+            Quaternion quaternion1 = Vec3f.POSITIVE_X.getDegreesQuaternion(-entityIn.getPitch());
+            Quaternion quaternion2 = Vec3f.POSITIVE_Y.getDegreesQuaternion(-entityIn.getYaw());
+            quaternion.hamiltonProduct(quaternion1);
+            quaternion.hamiltonProduct(quaternion2);
+            PTS_modelViewStack.multiply(quaternion);
+            RenderSystem.setShader(GameRenderer::getRenderTypeEntityCutoutNoNullShader);
+            model.model.Render(entityIn, entityYaw, 0.0f, new Vec3f(0.0f,0.0f,0.0f), PTS_modelViewStack, packedLightIn);
+            PTS_modelViewStack.pop();
+        }else{
+            matrixStackIn.scale(size[0], size[0], size[0]);
+            RenderSystem.setShader(GameRenderer::getRenderTypeEntityCutoutNoNullShader);
+            model.model.Render(entityIn, bodyYaw, bodyPitch, entityTrans, matrixStackIn, packedLightIn);
+        }
+        matrixStackIn.pop();
     }
 
     float[] sizeOfModel(MMDModelManager.Model model){
@@ -97,6 +107,14 @@ public class KAIMyEntityRenderer<T extends Entity> extends EntityRenderer<T> {
         size[0] = (model.properties.getProperty("size") == null) ? 1.0f : Float.valueOf(model.properties.getProperty("size"));
         size[1] = (model.properties.getProperty("size_in_inventory") == null) ? 1.0f : Float.valueOf(model.properties.getProperty("size_in_inventory"));
         return size;
+    }
+
+    void AnimStateChangeOnce(MMDModelManager.ModelWithEntityData model, MMDModelManager.EntityData.EntityState targetState, Integer layer) {
+        String Property = MMDModelManager.EntityData.stateProperty.get(targetState);
+        if (model.entityData.stateLayers[layer] != targetState) {
+            model.entityData.stateLayers[layer] = targetState;
+            model.model.ChangeAnim(MMDAnimManager.GetAnimModel(model.model, Property), layer);
+        }
     }
 
     @Override
